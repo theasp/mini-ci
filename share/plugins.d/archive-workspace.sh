5ccc6e597161a6607e64cf740a1b302b1622c079
Andrew Phillips <theasp@gmail.com>
2014-12-22 10:41:33 -0400

    Added emacs indention settings

--- share/plugins.d/archive-workspace.sh
+++ share/plugins.d/archive-workspace.sh
@@ -1,12 +1,18 @@
 declare BUILD_ARCHIVE_WORKSPACE
 
 plugin_on_load_config_pre_archive_workspace() {
-    BUILD_ARCHIVE_WORKSPACE="no"
+  BUILD_ARCHIVE_WORKSPACE="no"
 }
 
 plugin_on_tasks_finish_post_archive_workspace() {
-    if [[ "$BUILD_ARCHIVE_WORKSPACE" = "yes" ]]; then
-        log "Archiving workspace for build $BUILD_NUMBER"
-        cp -a $WORKSPACE $BUILD_OUTPUT_DIR/workspace
-    fi
+  if [[ "$BUILD_ARCHIVE_WORKSPACE" = "yes" ]]; then
+    log "Archiving workspace for build $BUILD_NUMBER"
+    cp -a $WORKSPACE $BUILD_OUTPUT_DIR/workspace
+  fi
 }
+
+# Local Variables:
+# sh-basic-offset: 2
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# End:
