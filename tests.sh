d33d1075edfe357488056a49dfad617aec2b828a
Andrew Phillips <theasp@gmail.com>
2014-12-23 16:27:19 -0400

    Set MINI_CI_DIR if not set

--- tests.sh
+++ tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+export MINI_CI_DIR=${MINI_CI_DIR:-"$PWD/share"}
+
 TEST_DIR=./tests
 for test in $(ls -1 $TEST_DIR | grep -E -e '^[a-zA-Z0-9_-]+$' | sort); do
     $TEST_DIR/$test
